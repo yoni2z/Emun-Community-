1eec024428892a7d1e5e0bc7381ac1d8f980bf3a
Gutu_R <adiregassa@gmail.com>
2025-01-02 10:30:48 +0000

    Added blog page

--- Frontend/Host/src/pages/Blogs.jsx
+++ Frontend/Host/src/pages/Blogs.jsx
@@ -1,13 +1,94 @@
 import React from 'react'
-import TitleBanner from '../components/blogs/TitleBanner'
+import TitleBanner from '../components/blog/TitleBanner'
+import BlogRight from '../components/blog/BlogRightColumn';
+import BlogPosts from '../components/blog/BlogPosts';
+
 const backgroundImage = "https://images.pexels.com/photos/9968379/pexels-photo-9968379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+const blogPosts = [
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-08_medium.jpg',
+    category: 'Hope',
+    postTitle: 'First Full-Width Post',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-09_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Second Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-07_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Third Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-06_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Fourth Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-06_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Fourth Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-06_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Fourth Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-06_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Fourth Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-06_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Fourth Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-06_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Fourth Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    postImage: 'https://demo.joomshaper.com/2017/hope/images/2017/09/19/blog-06_medium.jpg',
+    category: 'Hope',
+    postTitle: 'Fourth Post Title',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  
+];
 
 function blogs() {
   return ( 
-    <div>
+    <div className='bg-[#f3f5f7]'>
+
       <TitleBanner 
       title={'Blog'}
-      backgroundImage={backgroundImage}/>    </div>
+      backgroundImage={backgroundImage}/>
+
+      <div className='w-full grid grid-cols-3 gap-[2vw] py-[7%] px-[5%]'>
+        <div className='col-span-2'>
+          <BlogPosts posts = {blogPosts} />
+
+        </div>
+
+        <div className='col-span-1'>
+          <BlogRight />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- Frontend/Host/tailwind.config.js
+++ Frontend/Host/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#00bbff",
+        secondary: "#949494",
+        background: "#F4F5F7",
+      },
+    },
   },
   plugins: [],
-}
+};
