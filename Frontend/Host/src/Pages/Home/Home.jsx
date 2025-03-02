e239265b17b75bdb3e139385a99e9015accf7fd8
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-03-02 13:39:33 +0300

    Removed the counter, made the hero text bold and fixed donate now button padding on the home page

--- Frontend/Host/src/Pages/Home/Home.jsx
+++ Frontend/Host/src/Pages/Home/Home.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import Hero from '../../components/Hero/Hero';
-import { Partner } from '../../components/Partner/Partner';
-import { About } from '../../components/About/About';
-import { Donate } from '../../components/Donate/Donate';
-import { Videos } from '../../components/Video/Videos';
-import { Features } from '../../components/Features/Features';
-import { Statistics } from '../../components/Statistics/Statistics';
-import { Footer } from '../../components/Footer/Footer';
-import { Trademark } from '../../components/Trademark/Trademark';
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Hero from "../../components/Hero/Hero";
+import { Partner } from "../../components/Partner/Partner";
+import { About } from "../../components/About/About";
+import { Donate } from "../../components/Donate/Donate";
+import { Videos } from "../../components/Video/Videos";
+import { Features } from "../../components/Features/Features";
+import { Statistics } from "../../components/Statistics/Statistics";
+import { Footer } from "../../components/Footer/Footer";
+import { Trademark } from "../../components/Trademark/Trademark";
 import "./styles.css";
 
 export const Home = () => {
   return (
     <>
-       <Hero />
-       {/* <Partner /> */}
-       <About />
-       <Donate />
-       <Videos />
-       <Features />
-       <Statistics />
+      <Hero />
+      {/* <Partner /> */}
+      <About />
+      <Donate />
+      <Videos />
+      <Features />
+      {/* <Statistics /> */}
     </>
-  )
+  );
 };
